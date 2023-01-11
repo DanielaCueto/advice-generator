dfa93971ef2a32c9dc0e0d527f205d6d1f7dacb7
Daniela <danisalazarcueto7@gmail.com>
2023-01-11 16:12:08 +0000

    add interaction and styles

--- js/api.js
+++ js/api.js
@@ -1,1 +1,40 @@
 'use strict'
+let textAdvice = document.querySelector('.js-advice')
+console.log(textAdvice);
+const numberOfAdvice = document.querySelector('.js-numberofAdvice')
+console.log(numberOfAdvice);
+const diceContentor = document.querySelector('.js-contentorEvent')
+console.log(diceContentor);
+const mainContentor = document.querySelector('.main')
+
+
+
+function generateAdvice (event){
+    doingFetch()
+    console.log(event);
+    const clicked = event.currentTarget
+    //console.log(clicked);
+    
+    return clicked;
+
+
+}
+
+
+function doingFetch (){
+    //mainContentor.classList.add('loading');
+    fetch("https://api.adviceslip.com/advice")
+    .then((response)=> 
+    response.json())
+   .then((data)=> {
+        console.log(data.slip.advice);
+        numberOfAdvice.textContent = ` ADVICE #${data.slip.id}`
+        textAdvice.textContent = data.slip.advice
+        //mainContentor.classList.remove('loading')
+
+    })
+}
+
+doingFetch()
+
+diceContentor.addEventListener('click', generateAdvice)
--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 'use strict'
 
-const numberOfAdvice = document.querySelector('.js-numberofAdvice')
-console.log(numberOfAdvice);
-const textAdvice = document.querySelector('.js-advice')
-console.log(textAdvice);
-const diceContentor = document.querySelector('.js-contentorEvent')
-console.log(diceContentor);
 
 
-function generateAdvice (event){
-    console.log(event);
-    const clicked = event.currentTarget
-    console.log(clicked);
-
-}
-
-
-diceContentor.addEventListener('click', generateAdvice)
